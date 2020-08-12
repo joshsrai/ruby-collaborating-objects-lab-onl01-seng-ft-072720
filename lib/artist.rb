b43ffd97a1093bc5b777dbb6972db2a992dddf1c
Josh Raiborde <joshsrai@yahoo.com>
2020-08-12 20:17:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -26,6 +26,16 @@ class Artist
     @@all << self
   end
  
+ def self.new_by_filename(file)
+   data = file.split("/").last
+   artist = data.split(" - ")[0].strip
+   Artist.find_or_create_by_name(artist)
+ end
+ 
+ def self.find_or_create_by_name(name)
+   artist = @@all.find{|artist| artist.name == name}
+   !!artist ? artist : Artist.new(name)
+ end
  
   
   
